a840a473032b942b1b6310be814d7086a49cdd68
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-07-21 08:19:44 +0200

    First attempt at hosting docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,19 @@
 using Scapin
 using Documenter
 
-DocMeta.setdocmeta!(Scapin, :DocTestSetup, :(using Scapin); recursive=true)
+DocMeta.setdocmeta!(Scapin, :DocTestSetup, :(using Scapin); recursive = true)
 
 makedocs(;
-    modules=[Scapin],
-    authors="Sébastien Brisard <sbrisard@users.noreply.github.com> and contributors",
-    repo="https://github.com/sbrisard/Scapin.jl/blob/{commit}{path}#{line}",
-    sitename="Scapin.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        assets=String[],
+    modules = [Scapin],
+    authors = "Sébastien Brisard <sbrisard@users.noreply.github.com> and contributors",
+    repo = "https://github.com/sbrisard/Scapin.jl/blob/{commit}{path}#{line}",
+    sitename = "Scapin.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://sbrisard.github.io/Scapin.jl",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
+
+deploydocs(; repo = "github.com/sbrisard/Scapin.jl", versions = nothing)
