80cd82af5ba4ca6ad8c147089c2231fd3b0369be
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-10-15 08:11:04 +0200

    New function `dimensionality`

--- src/Scapin.jl
+++ src/Scapin.jl
@@ -3,6 +3,15 @@ module Scapin
 using Base.Iterators
 using LinearAlgebra
 
+"""
+    dimensionality(op)
+
+Return the number of dimensions of the physical space that `op` operates on.
+"""
+function dimensionality end
+
+export dimensionality
+
 include("Elasticity.jl")
 include("Grid.jl")
 include("Brick.jl")
