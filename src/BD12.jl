88939e30a3bbaaca86042f9423cbedaa3f2fd016
Sébastien Brisard <sbrisard@users.noreply.github.com>
2022-08-19 09:36:19 +0200

    JuliaFormatter

--- src/BD12.jl
+++ src/BD12.jl
@@ -12,30 +12,30 @@ struct BrisardDormieux2012{CONT_OP,d,T}
     F::CONT_OP
     N::NTuple{d,Int}
     C::NTuple{d,Vector{T}}
-    function BrisardDormieux2012{CONT_OP,d,T}(F::CONT_OP, N) where {CONT_OP,d, T}
+    function BrisardDormieux2012{CONT_OP,d,T}(F::CONT_OP, N) where {CONT_OP,d,T}
         # TODO Check that d == dimensionality(F) == ndims(N)
         # TODO Check that T == eltype(CONT_OP)
         # TODO Check that ndims(F) == 2
         C = Tuple([cos(π * nᵢ / (2Nᵢ)) for nᵢ = 0:Nᵢ] for Nᵢ in N)
-        new{CONT_OP,d, T}(F, N, C)
+        new{CONT_OP,d,T}(F, N, C)
     end
 end
 
 BrisardDormieux2012(F::CONT_OP, N) where {CONT_OP} = BrisardDormieux2012{CONT_OP,dimensionality(CONT_OP),eltype(CONT_OP)}(F, N)
 
-Base.eltype(::Type{BrisardDormieux2012{CONT_OP, d,T}}) where {CONT_OP,d,T} =
+Base.eltype(::Type{BrisardDormieux2012{CONT_OP,d,T}}) where {CONT_OP,d,T} =
     eltype(CONT_OP)
 Base.ndims(::Type{BrisardDormieux2012{CONT_OP,d,T}}) where {CONT_OP,d,T} = 2
 Base.ndims(::BrisardDormieux2012{CONT_OP,d,T}) where {CONT_OP,d,T} = 2
 
-Base.size(F_N::BrisardDormieux2012{CONT_OP, d, T}) where {CONT_OP,d, T} = size(F_N.F)
-Base.size(F_N::BrisardDormieux2012{CONT_OP, d, T}, n::Int) where {CONT_OP, d, T} = size(F_N.F, n)
+Base.size(F_N::BrisardDormieux2012{CONT_OP,d,T}) where {CONT_OP,d,T} = size(F_N.F)
+Base.size(F_N::BrisardDormieux2012{CONT_OP,d,T}, n::Int) where {CONT_OP,d,T} = size(F_N.F, n)
 
-Scapin.dimensionality(::Type{BrisardDormieux2012{CONT_OP, d, T}}) where {CONT_OP, d, T} = d
+Scapin.dimensionality(::Type{BrisardDormieux2012{CONT_OP,d,T}}) where {CONT_OP,d,T} = d
 
-Scapin.grid_size(F_N::BrisardDormieux2012{CONT_OP, d, T}) where {CONT_OP, d, T} = F_N.N
+Scapin.grid_size(F_N::BrisardDormieux2012{CONT_OP,d,T}) where {CONT_OP,d,T} = F_N.N
 
-function Scapin.apply_fourier!(ŷ, F_N::BrisardDormieux2012{CONT_OP, d, T}, n, x̂) where{CONT_OP, d, T}
+function Scapin.apply_fourier!(ŷ, F_N::BrisardDormieux2012{CONT_OP,d,T}, n, x̂) where {CONT_OP,d,T}
     N = grid_size(F_N)
     ŷ[:] .= zero(T)
 
