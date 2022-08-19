1802f735f2f4d0ad490f43a6640c5c47aac9c2d0
Sébastien Brisard <sbrisard@users.noreply.github.com>
2022-08-19 09:32:28 +0200

    Demo of the BD12 discretization (now working!)

--- playground/demo_bd12.jl
+++ playground/demo_bd12.jl
@@ -8,15 +8,13 @@ const T = Float64
 const d = 2
 
 C = Hooke{d,T}(1.0, 0.3)
-N = (128, 256)
+N = (256, 256)
 
 Γ = GreenOperatorHooke{d,T}(C)
 Γ_N = BrisardDormieux2012(Γ, N)
 
 τ = zeros(T, 3, N...)
-τ[3, 1, 1, 1] = 1
-
+τ[3, 1:(N[1]÷4), 1:(N[2]÷4)] .= 1
 ε = apply(Γ_N, τ)
 
-# heatmap(ε[3, :, :])
-# save("eps_xy-$(N[1])x$(N[2]).png", fig)
+heatmap(real.(ε[3, :, :]), c=:viridis)
