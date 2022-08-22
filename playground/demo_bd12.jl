6648cd40e0b01b784c7a9e483f7d16e8904d342e
Sébastien Brisard <sbrisard@users.noreply.github.com>
2022-08-22 09:36:49 +0200

    Convergence analysis of BD12 discretization

--- playground/demo_bd12.jl
+++ playground/demo_bd12.jl
@@ -1,20 +1,48 @@
+using ImageTransformations
+using Interpolations
 using LinearAlgebra
 using Plots
 using Scapin
 using Scapin.Elasticity
 using Scapin.BD12
+using Scapin.Bri17
 
 const T = Float64
 const d = 2
 
 C = Hooke{d,T}(1.0, 0.3)
-N = (256, 256)
-
 Γ = GreenOperatorHooke{d,T}(C)
-Γ_N = BrisardDormieux2012(Γ, N)
 
-τ = zeros(T, 3, N...)
-τ[3, 1:(N[1]÷4), 1:(N[2]÷4)] .= 1
-ε = apply(Γ_N, τ)
+N_coarse = (4, 4)
+r_max = 9
+τ_coarse = zeros(T, size(Γ, 2), N_coarse...)
+τ_coarse[end, 1, 1] = 1.0
 
-heatmap(real.(ε[3, :, :]), c=:viridis)
+N_ref = (2^r_max) .* N_coarse
+ε_ref = Array{T}(undef, size(Γ, 2), N_ref...)
+
+results = Dict()
+
+for r ∈ 0:r_max
+    N = (2^r) .* N_coarse
+    𝒩 = CartesianIndices(N)
+    h = 1.0 ./ N
+
+    τ = zeros(T, size(Γ, 2), N...)
+    τ[end, fill(1:2^r, d)...] .= one(T)
+
+    Γ_N = BrisardDormieux2012(Γ, N)
+    ε = apply(Γ_N, τ)
+    # display(heatmap(real.(ε[3, :, :]), c=:viridis))
+    results[r] = ε
+
+    if r == r_max
+        # Compute reference solution
+        global ε_ref = apply(DiscreteGreenOperatorBri17{d,T}(C, N, h), τ)
+    end
+end
+
+for r ∈ 0:(r_max-1)
+    ε = imresize(results[r], size(ε_ref)..., method=BSpline(Constant()))
+    println(norm(ε - ε_ref))
+end
