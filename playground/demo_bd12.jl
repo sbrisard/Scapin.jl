1eb7dc382e295511aa58f68759c84744c4e83d58
Sébastien Brisard <sbrisard@users.noreply.github.com>
2022-08-24 09:03:10 +0200

    Further code factorization

--- playground/demo_bd12.jl
+++ playground/demo_bd12.jl
@@ -7,11 +7,6 @@ using Scapin.Elasticity
 using Scapin.BD12
 using Scapin.Bri17
 
-const T = Float64
-const d = 2
-
-C = Hooke{d,T}(1.0, 0.3)
-Γ = GreenOperatorHooke{d,T}(C)
 
 function refine_and_apply(Γ, τ, r, ctor)
     T = eltype(Γ)
@@ -21,57 +16,50 @@ function refine_and_apply(Γ, τ, r, ctor)
     return apply(Γ_N, τ_N)
 end
 
+function convergence_analysis(Γ, τ_coarse, r_max, ctor)
+    N_ref = (2^r_max) .* size(τ_coarse)
+    ε_ref = Array{T}(undef, size(Γ, 2), N_ref...)
+    ε_ref = refine_and_apply(
+        Γ,
+        τ_coarse,
+        r_max + 1,
+        (Γ, N) -> DiscreteGreenOperatorBri17{d,T}(Γ.C, N, Tuple(fill(one(T), d))),
+    )
+
+    [refine_and_apply(Γ, τ_coarse, r, ctor) for r ∈ 0:r_max], ε_ref
+end
+
+const T = Float64
+const d = 2
+
+C = Hooke{d,T}(1.0, 0.3)
+Γ = GreenOperatorHooke{d,T}(C)
+ctor = BrisardDormieux2012
 
 N_coarse = (4, 4)
 r_max = 8
 τ_coarse = zeros(T, size(Γ, 2), N_coarse...)
 τ_coarse[end, 1, 1] = 1.0
 
-N_ref = (2^r_max) .* N_coarse
-ε_ref = Array{T}(undef, size(Γ, 2), N_ref...)
 
-results = Dict()
-for r ∈ 0:r_max
-    results[r] = refine_and_apply(Γ, τ_coarse, r, BrisardDormieux2012)
-end
 
-ε_ref = refine_and_apply(Γ, τ_coarse, r_max + 1, (Γ, N) -> DiscreteGreenOperatorBri17{d,T}(Γ.C, N, Tuple(fill(one(T), d))))
-
-# for r ∈ 0:r_max+1
-#     N = (2^r) .* N_coarse
-#     𝒩 = CartesianIndices(N)
-#     h = 1.0 ./ N
-
-#     τ = zeros(T, size(Γ, 2), N...)
-#     τ[end, fill(1:2^r, d)...] .= one(T)
-
-#     if r <= r_max
-#         Γ_N = BrisardDormieux2012(Γ, N)
-#         ε = apply(Γ_N, τ)
-#         # display(heatmap(real.(ε[3, :, :]), c=:viridis))
-#         results[r] = ε
-#     else
-#         # Compute reference solution
-#         global ε_ref = apply(DiscreteGreenOperatorBri17{d,T}(C, N, h), τ)
-#     end
-# end
+(ε, ε_ref) = convergence_analysis(Γ, τ_coarse, r_max, ctor)
 
 x = [(2^r) .* N_coarse[1] for r ∈ 0:r_max]
-err = zeros(Float64, r_max + 1)
-for r ∈ 0:r_max
-    ε = imresize(results[r], size(ε_ref)..., method = BSpline(Constant()))
-    err[r+1] = norm(ε - ε_ref) / norm(ε_ref)
-end
+y = [
+    norm(imresize(ε_r, size(ε_ref)..., method = BSpline(Constant())) - ε_ref) / norm(ε_ref) for ε_r ∈ ε]
 
 plot(
     x,
-    err,
+    y,
     xscale = :log10,
     yscale = :log10,
     markershape = :circle,
+    markerstrokecolor = :white,
     xlabel = "Grid size, N",
     ylabel = "Relative L² error",
-    label=""
+    label = "BD12",
 )
+plot!(x, 10 .* x .^ (-1), label="∝ N⁻¹")
 
-savefig("convergence.png")
+savefig("convergence.pdf")
