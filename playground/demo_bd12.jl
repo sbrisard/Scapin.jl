6a89af1b4cca865676d6c28c59253e7e75f02882
Sébastien Brisard <sbrisard@users.noreply.github.com>
2022-08-25 08:59:57 +0200

    Use new module `ConvergenceAnalysis`

--- playground/demo_bd12.jl
+++ playground/demo_bd12.jl
@@ -1,32 +1,30 @@
-using ImageTransformations
-using Interpolations
 using LinearAlgebra
 using Plots
 using Scapin
 using Scapin.Elasticity
 using Scapin.BD12
 using Scapin.Bri17
-
-
-function refine_and_apply(Γ, τ, r, ctor)
-    T = eltype(Γ)
-    N = (2^r) .* (size(τ)[2:end])
-    Γ_N = ctor(Γ, N)
-    τ_N = imresize(τ, size(Γ, 2), N..., method = BSpline(Constant()))
-    return apply(Γ_N, τ_N)
-end
+using Scapin.ConvergenceAnalysis
 
 function convergence_analysis(Γ, τ_coarse, r_max, ctor)
-    N_ref = (2^r_max) .* size(τ_coarse)
-    ε_ref = Array{T}(undef, size(Γ, 2), N_ref...)
-    ε_ref = refine_and_apply(
-        Γ,
-        τ_coarse,
-        r_max + 1,
-        (Γ, N) -> DiscreteGreenOperatorBri17{d,T}(Γ.C, N, Tuple(fill(one(T), d))),
-    )
+    # TODO Move to signature of function
+    d = dimensionality(Γ)
+    T = eltype(Γ)
+    N_ref = (2^(r_max + 1)) .* size(τ_coarse)[2:end]
+    h = Tuple(fill(one(T), d))
 
-    [refine_and_apply(Γ, τ_coarse, r, ctor) for r ∈ 0:r_max], ε_ref
+    Γ_ref = DiscreteGreenOperatorBri17{d,T}(Γ.C, N_ref, h)
+
+    ε_ref = refine_and_apply(Γ_ref, τ_coarse)
+
+    ε = []
+    for r ∈ 0:r_max
+        N = (2^r) .* (size(τ_coarse)[2:end])
+        Γ_N = ctor(Γ, N)
+        push!(ε, refine_and_apply(Γ_N, τ_coarse))
+    end
+
+    return ε, ε_ref
 end
 
 const T = Float64
@@ -41,13 +39,12 @@ r_max = 8
 τ_coarse = zeros(T, size(Γ, 2), N_coarse...)
 τ_coarse[end, 1, 1] = 1.0
 
-
-
 (ε, ε_ref) = convergence_analysis(Γ, τ_coarse, r_max, ctor)
 
 x = [(2^r) .* N_coarse[1] for r ∈ 0:r_max]
 y = [
-    norm(imresize(ε_r, size(ε_ref)..., method = BSpline(Constant())) - ε_ref) / norm(ε_ref) for ε_r ∈ ε]
+    norm(imresize(ε_r, size(ε_ref)..., method = BSpline(Constant())) - ε_ref) / norm(ε_ref) for ε_r ∈ ε
+]
 
 plot(
     x,
@@ -60,6 +57,6 @@ plot(
     ylabel = "Relative L² error",
     label = "BD12",
 )
-plot!(x, 10 .* x .^ (-1), label="∝ N⁻¹")
+plot!(x, 10 .* x .^ (-1), label = "∝ N⁻¹")
 
 savefig("convergence.pdf")
