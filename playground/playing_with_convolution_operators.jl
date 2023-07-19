7c82d3f3d5a1f6694cdf309a04ed7c1eac19eb11
Sébastien Brisard <sbrisard@users.noreply.github.com>
2023-07-19 06:58:29 +0200

    Check that implemented LinearMap works with Krylov.cg

--- playground/playing_with_convolution_operators.jl
+++ playground/playing_with_convolution_operators.jl
@@ -1,69 +1,71 @@
 using BenchmarkTools
 using FFTW
+using Krylov
 using LinearAlgebra
 using LinearMaps
+using Statistics
 
-abstract type AbstractGridOperator{T, N} <: LinearMap{T} end
+abstract type AbstractGridOperator{T,N} <: LinearMap{T} end
 
-grid_size(L::AbstractGridOperator{T, N}, d) where {T, N} = d::Integer <= N ? grid_size(L)[d] : 1
-grid_step(L::AbstractGridOperator{T, N}, d) where {T, N} = d::Integer <= N ? grid_step(L)[d] : zero(T)
+grid_size(L::AbstractGridOperator{T,N}, d) where {T,N} = d::Integer <= N ? grid_size(L)[d] : 1
+grid_step(L::AbstractGridOperator{T,N}, d) where {T,N} = d::Integer <= N ? grid_step(L)[d] : zero(T)
 
-struct MinusLaplaceOperator{T, DIM} <: AbstractGridOperator{T, DIM}
-    size::NTuple{2, Int}
-    grid_size::NTuple{DIM, Int}
-    grid_step::NTuple{DIM, T}
-    function MinusLaplaceOperator{T, DIM}(N::NTuple{DIM, Integer}, h::NTuple{DIM, T}) where {T, DIM}
-        num_cells = prod(N)
-        return new{T, DIM}((num_cells, num_cells), N, h)
-    end
+struct MinusLaplaceOperator{T,DIM} <: AbstractGridOperator{T,DIM}
+        size::NTuple{2,Int}
+        grid_size::NTuple{DIM,Int}
+        grid_step::NTuple{DIM,T}
+        function MinusLaplaceOperator{T,DIM}(N::NTuple{DIM,Integer}, h::NTuple{DIM,T}) where {T,DIM}
+                num_cells = prod(N)
+                return new{T,DIM}((num_cells, num_cells), N, h)
+        end
 end
 
 Base.size(L::MinusLaplaceOperator) = L.size
 grid_size(L::MinusLaplaceOperator) = L.grid_size
 grid_step(L::MinusLaplaceOperator) = L.grid_step
 
-function LinearMaps._unsafe_mul!(v::AbstractVecOrMat, L::MinusLaplaceOperator{T, 2}, u::AbstractVecOrMat) where T
-    (Nx, Ny) = grid_size(L)
-    (hx², hy²) = grid_step(L) .^ 2
-    u_arr = reshape(u, Nx, Ny)
-    v_arr = reshape(v, Nx, Ny)
+function LinearMaps._unsafe_mul!(v::AbstractVecOrMat, L::MinusLaplaceOperator{T,2}, u::AbstractVecOrMat) where {T}
+        (Nx, Ny) = grid_size(L)
+        (hx², hy²) = grid_step(L) .^ 2
+        u_arr = reshape(u, Nx, Ny)
+        v_arr = reshape(v, Nx, Ny)
 
-    for j₀ ∈ 1:Ny
-        j₋₁ = j₀ == 1 ? Ny : j₀ - 1
-        j₊₁ = j₀ == Ny ? 1 : j₀ + 1
-        for i₀ ∈ 1:Nx
-            i₋₁ = i₀ == 1 ? Nx : i₀ - 1
-            i₊₁ = i₀ == Nx ? 1 : i₀ + 1
-            v_arr[i₀, j₀] = -(
-                (u_arr[i₊₁, j₀] - 2 * u_arr[i₀, j₀] + u_arr[i₋₁, j₀]) / hx² +
-                    (u_arr[i₀, j₊₁] - 2 * u_arr[i₀, j₀] + u_arr[i₀, j₋₁]) / hy²
-            )
+        for j₀ ∈ 1:Ny
+                j₋₁ = j₀ == 1 ? Ny : j₀ - 1
+                j₊₁ = j₀ == Ny ? 1 : j₀ + 1
+                for i₀ ∈ 1:Nx
+                        i₋₁ = i₀ == 1 ? Nx : i₀ - 1
+                        i₊₁ = i₀ == Nx ? 1 : i₀ + 1
+                        v_arr[i₀, j₀] = -(
+                                (u_arr[i₊₁, j₀] - 2 * u_arr[i₀, j₀] + u_arr[i₋₁, j₀]) / hx² +
+                                (u_arr[i₀, j₊₁] - 2 * u_arr[i₀, j₀] + u_arr[i₀, j₋₁]) / hy²
+                        )
+                end
         end
-    end
-    return v
+        return v
 end
 
 LinearMaps.MulStyle(L::MinusLaplaceOperator) = FiveArg()
 
-function LinearMaps._unsafe_mul!(v::AbstractVector, L::MinusLaplaceOperator{T, 2}, u::AbstractVector, α::Number, β::Number) where T
-    (Nx, Ny) = grid_size(L)
-    (hx², hy²) = grid_step(L) .^ 2
-    u_arr = reshape(u, Nx, Ny)
-    v_arr = reshape(v, Nx, Ny)
+function LinearMaps._unsafe_mul!(v::AbstractVector, L::MinusLaplaceOperator{T,2}, u::AbstractVector, α::Number, β::Number) where {T}
+        (Nx, Ny) = grid_size(L)
+        (hx², hy²) = grid_step(L) .^ 2
+        u_arr = reshape(u, Nx, Ny)
+        v_arr = reshape(v, Nx, Ny)
 
-    for j₀ ∈ 1:Ny
-        j₋₁ = j₀ == 1 ? Ny : j₀ - 1
-        j₊₁ = j₀ == Ny ? 1 : j₀ + 1
-        for i₀ ∈ 1:Nx
-            i₋₁ = i₀ == 1 ? Nx : i₀ - 1
-            i₊₁ = i₀ == Nx ? 1 : i₀ + 1
-            δ²_yy_u = (u_arr[i₀, j₊₁] - 2 * u_arr[i₀, j₀] + u_arr[i₀, j₋₁]) / hy²
-            δ²_xx_u = (u_arr[i₊₁, j₀] - 2 * u_arr[i₀, j₀] + u_arr[i₋₁, j₀]) / hx²
-            Δu = δ²_xx_u + δ²_yy_u
-            v_arr[i₀, j₀] = α * (-Δu) + β * v_arr[i₀, j₀]
+        for j₀ ∈ 1:Ny
+                j₋₁ = j₀ == 1 ? Ny : j₀ - 1
+                j₊₁ = j₀ == Ny ? 1 : j₀ + 1
+                for i₀ ∈ 1:Nx
+                        i₋₁ = i₀ == 1 ? Nx : i₀ - 1
+                        i₊₁ = i₀ == Nx ? 1 : i₀ + 1
+                        δ²_yy_u = (u_arr[i₀, j₊₁] - 2 * u_arr[i₀, j₀] + u_arr[i₀, j₋₁]) / hy²
+                        δ²_xx_u = (u_arr[i₊₁, j₀] - 2 * u_arr[i₀, j₀] + u_arr[i₋₁, j₀]) / hx²
+                        Δu = δ²_xx_u + δ²_yy_u
+                        v_arr[i₀, j₀] = α * (-Δu) + β * v_arr[i₀, j₀]
+                end
         end
-    end
-    return v
+        return v
 end
 
 LinearAlgebra.issymmetric(L::MinusLaplaceOperator) = true
@@ -74,16 +76,27 @@ Ly = 5.0
 Nx = 40
 Ny = 80
 
-L = MinusLaplaceOperator{Float64, 2}((Nx, Ny), (Lx / Nx, Ly/Ny))
+L = MinusLaplaceOperator{Float64,2}((Nx, Ny), (Lx / Nx, Ly / Ny))
 
-ux = [sin(π * (n + 0.5) / Nx) for n = 0:(Nx-1)]
-uy = [sin(π * (n + 0.5) / Ny) for n = 0:(Ny-1)]
-u = ux .* uy'
-u_vec = reshape(u, :)
-v_vec = L * u_vec
-v = reshape(v_vec, grid_size(L))
+# ux = [sin(π * (n + 0.5) / Nx) for n = 0:(Nx-1)]
+# uy = [sin(π * (n + 0.5) / Ny) for n = 0:(Ny-1)]
+# u = ux .* uy'
+# u_vec = reshape(u, :)
+# v_vec = L * u_vec
+# v = reshape(v_vec, grid_size(L))
 
-α = 2.0
-β = 3.0
+# α = 2.0
+# β = 3.0
 
-@benchmark mul!(v_vec, L, u_vec, α, β)
+# @benchmark mul!(v_vec, L, u_vec, α, β)
+
+u_true = rand(Float64, size(L, 2))
+f = L * u_true
+
+rtol = 1e-10
+atol = 1e-10
+(u, stats) = cg(L, f, atol=atol, rtol=rtol)
+
+v = u - u_true
+r = abs.(v .- mean(v))
+@assert all(isapprox.(r, 0.0, rtol=10rtol, atol=10atol))
